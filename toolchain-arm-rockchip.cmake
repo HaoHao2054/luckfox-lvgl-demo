fd00447837dadce285bb8ed5cce8db60101e550e
yuanhao <2054190139@qq.com>
2025-05-31 00:13:07 +0800

    优化工程结构

--- toolchain-arm-rockchip.cmake
+++ toolchain-arm-rockchip.cmake
@@ -8,12 +8,13 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CROSS_COMPILER_PREFIX /home/hao/luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-)
 
 set(CMAKE_C_COMPILER ${CROSS_COMPILER_PREFIX}gcc)
-set(CMAKE_CXX_COMPILER ${CROSS_COMPILER_PREFIX}g++) # For C++ files if any (e.g., bsp/keypad)
+set(CMAKE_CXX_COMPILER ${CROSS_COMPILER_PREFIX}g++)
 
 # Set the sysroot if necessary (often needed for includes and libraries)
 # Example: set(CMAKE_SYSROOT /home/hao/luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/arm-rockchip830-linux-uclibcgnueabihf/sysroot)
 # If your toolchain handles sysroot implicitly, you might not need this.
 # Adjust the path above if your sysroot is located elsewhere within the toolchain directory.
+set(CMAKE_SYSROOT /home/hao/luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/arm-rockchip830-linux-uclibcgnueabihf/sysroot)
 
 # Configure find commands
 # set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
